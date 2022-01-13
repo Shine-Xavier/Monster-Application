accc95bee8a1586fc7b9d43bb361afed58398d9a
Shine-Xavier <shinexavier94@gmail.com>
2022-01-13 10:27:07 +0530

    Updates

---
+++ precache-manifest.bb8b8d4eca2028ec091ac318b1bd39da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71a8b38849e5a9f1604c599c781ae50f",
+    "url": "/Monster-Application/index.html"
+  },
+  {
+    "revision": "df6ddc16e8478ac86b45",
+    "url": "/Monster-Application/static/css/main.f251e27e.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/Monster-Application/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Monster-Application/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df6ddc16e8478ac86b45",
+    "url": "/Monster-Application/static/js/main.66e331c5.chunk.js"
+  },
+  {
+    "revision": "08a613ce7f5d4bc33427",
+    "url": "/Monster-Application/static/js/runtime-main.412f5e1e.js"
+  }
+]);
